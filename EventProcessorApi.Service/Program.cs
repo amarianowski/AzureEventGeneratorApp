319c9be76e9de51bacc6f720f2356e84f7debd94
Adam Marianowski <a.marianowski@outlook.com>
2026-09-12 02:04:16 +0100

    web api boilerplate

--- EventProcessorApi.Service/Program.cs
+++ EventProcessorApi.Service/Program.cs
@@ -1,41 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using AzureEventGeneratorApp.Contracts;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/telemetry", (TelemetryReading reading) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return Results.Accepted("Received telemetry reading");
 })
-.WithName("GetWeatherForecast");
+.WithName("Push arctic telemetry readings");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
